d35e3b2dfa30a415f3682da6674ddb0fd9760a99
Ian Fox <code@whatthefox.dev>
2026-07-06 12:01:15 +0200

    Add pre-commit hook

--- README.md
+++ README.md
@@ -6,3 +6,7 @@ Documenting my journey to setting up a home-lab fully managed by [nix](https://n
 
 - [Design Doc](./docs/DESIGN.md)
 - [Architecture](./docs/ARCHITECTURE.md)
+
+## Developing
+
+A [devshell](./lib/devshell.nix) with git hooks is provided for use with `nix develop`. This ensures `nix fmt` and `nix flake check` are run before a commit.
--- lib/devshell.nix
+++ lib/devshell.nix
@@ -2,11 +2,25 @@
 {
   imports = [ inputs.devshell.flakeModule ];
 
+  flake.description = "This is a test";
+
   perSystem =
     { pkgs, ... }:
     {
-      devshells.default = {
-        packages = [ pkgs.cowsay ];
-      };
+      devshells.default = (
+        { extraModulesPath, ... }@args:
+        {
+          # Ref: https://numtide.github.io/devshell/extending.html
+          # Ref: https://github.com/numtide/devshell/tree/main/extra
+          imports = [ "${extraModulesPath}/git/hooks.nix" ];
+
+          packages = [ pkgs.cowsay ];
+
+          git.hooks = {
+            enable = true;
+            pre-commit.text = "nix fmt && nix flake check";
+          };
+        }
+      );
     };
 }
